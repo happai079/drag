f5a02f1841a9f6a57bc10f3b36bd86efd973c0d4
happai079 <gksyddl22@gmail.com>
2022-11-13 14:09:13 +0900

    feat : mouse event 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import styled from 'styled-components';
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<AppContainer>
+			<h1>DRAG BOX</h1>
+			<DragBox
+				onMouseDown={() => {
+					const mouseMoveHandler = (e) => {
+						console.log(`mouse move x: ${e.screenX} y: ${e.screenY}`);
+					};
+
+					const mouseUpHandler = (e) => {
+						console.warn(`>>>> mouse up x: ${e.screenX} y: ${e.screenY}`);
+						document.removeEventListener('mousemove', mouseMoveHandler);
+					};
+
+					document.addEventListener('mousemove', mouseMoveHandler);
+					document.addEventListener('mouseup', mouseUpHandler, { once: true });
+				}}
+			/>
+		</AppContainer>
+	);
 }
+
+const AppContainer = styled.div`
+	height: 100vh;
+	margin: 0 auto;
+`;
+
+const DragBox = styled.div`
+	width: 100%;
+	height: 300px;
+	background-color: lightgray;
+	border-radius: 10px;
+`;
 
 export default App;
