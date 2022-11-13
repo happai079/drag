b5ffff4abf2d0ab3ee0a79c63a37745419eebddb
happai079 <gksyddl22@gmail.com>
2022-11-13 14:47:04 +0900

    feat : 드레그 위치 이동

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
 function App() {
+	const [{ x, y }, setPosition] = useState({ x: 0, y: 0 });
+
 	return (
 		<AppContainer>
 			<h1>DRAG BOX</h1>
-			<DragBox
-				onMouseDown={() => {
-					const mouseMoveHandler = (e) => {
-						console.log(`mouse move x: ${e.screenX} y: ${e.screenY}`);
-					};
+			<DragBox>
+				<Box
+					// 이동한 거리만큼 translate
+					style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
+					// mousedown - 마우스 클릭 이벤트
+					onMouseDown={(clickEvnet) => {
+						const mouseMoveHandler = (moveEvent) => {
+							// 클릭시 커서 위치 기준으로 이동한 거리 계산
+							const moveX = moveEvent.pageX - clickEvnet.pageX;
+							const moveY = moveEvent.pageY - clickEvnet.pageY;
 
-					const mouseUpHandler = (e) => {
-						console.warn(`>>>> mouse up x: ${e.screenX} y: ${e.screenY}`);
-						document.removeEventListener('mousemove', mouseMoveHandler);
-					};
+							setPosition({ x: x + moveX, y: y + moveY });
+						};
 
-					document.addEventListener('mousemove', mouseMoveHandler);
-					document.addEventListener('mouseup', mouseUpHandler, { once: true });
-				}}
-			/>
+						const mouseUpHandler = () => {
+							document.removeEventListener('mousemove', mouseMoveHandler);
+						};
+
+						// mousemove - 마우스 이동 이벤트
+						document.addEventListener('mousemove', mouseMoveHandler);
+						// mouseup - 마우스 클릭 해제 이벤트
+						document.addEventListener('mouseup', mouseUpHandler, { once: true });
+					}}
+				/>
+			</DragBox>
 		</AppContainer>
 	);
 }
@@ -29,10 +42,22 @@ const AppContainer = styled.div`
 `;
 
 const DragBox = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	width: 100%;
 	height: 300px;
 	background-color: lightgray;
 	border-radius: 10px;
+`;
+
+const Box = styled.div`
+	width: 100px;
+	height: 100px;
+	background-color: white;
+	box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);
+	border-radius: 20px;
+	cursor: move;
 `;
 
 export default App;
